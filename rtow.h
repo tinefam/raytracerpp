a35ef03ece7d6e2c65b6b684770cf5e8589f7ad9
tinefam <tinelli.francesco06@gmail.com>
2026-09-17 02:12:43 +0200

    Random_double: use thread_local instead of static

--- rtow.h
+++ rtow.h
@@ -17,8 +17,8 @@ using namespace std::numbers; // For pi
 
 inline double random_double()
 {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
+    thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    thread_local std::mt19937 generator;
     return distribution(generator);
 }
 
