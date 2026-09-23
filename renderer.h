88dd2bd2c7bb2f33237024c5afed5c2293f4e822
tinefam <tinelli.francesco06@gmail.com>
2026-09-23 12:00:20 +0200

    Improve renderer.h structure

--- renderer.h
+++ renderer.h
@@ -21,5 +21,20 @@ public:
 
 private:
     camera& cam;
-    world& world;
+    const hittable& world;
+
+    std::vector<color> grid; // Temporary vector to print threaded pixel
+    std::vector<ThreadTimerInfo> thread_timers; // Temporary vector to print thread timers
+
+    std::atomic<unsigned int> scan_remaining; // Multithread remaining scans counter
+
+    void division_thread()
+    {
+
+    }
+
+    void thread_grid(int r_start, int r_end, int n)
+    {
+
+    }
 };
