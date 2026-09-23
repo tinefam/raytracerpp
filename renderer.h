9d29b15ee29c4b9659922dc205157d5a8159c884
tinefam <tinelli.francesco06@gmail.com>
2026-09-23 11:34:54 +0200

    Create renderer.h

---
+++ renderer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <atomic>
+#include <vector>
+#include <thread>
+
+#include <camera.h>
+#include <color.h>
+#include <hittable.h>
+#include <timer.h>
+
+class Renderer
+{
+public:
+    // TODO: Constructor
+
+    // TODO: Render
+
+private:
+    // TODO: Members
+};
