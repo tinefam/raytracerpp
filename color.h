afa0bf117db2f5128360877571ba8b48f45c03db
tinefam <tinelli.francesco06@gmail.com>
2026-09-26 11:33:51 +0200

    Add raw_color_bytes function

--- color.h
+++ color.h
@@ -3,6 +3,8 @@
 #include "interval.h"
 #include "vec3.h"
 
+#include <array>
+
 using color = vec3;
 
 inline double linear_to_gamma(double linear_component)
@@ -15,7 +17,7 @@ inline double linear_to_gamma(double linear_component)
     return 0;
 }
 
-void write_color(std::ostream& out, const color& pixel_color)
+inline void write_color(std::ostream& out, const color& pixel_color)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
@@ -36,6 +38,24 @@ void write_color(std::ostream& out, const color& pixel_color)
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
-// New function
+inline std::array<unsigned char, 3> raw_color_bytes(const color& pixel_color)
+{
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
+    // Translate the [0,1] norm component values to the byte range [0,255]
+    static const interval intensity (0.000, 0.999);
+    unsigned char rbyte = int(256 * intensity.clamp(r));
+    unsigned char gbyte = int(256 * intensity.clamp(g));
+    unsigned char bbyte = int(256 * intensity.clamp(b));
+
+    return {rbyte, gbyte, bbyte};
+}
 
 
